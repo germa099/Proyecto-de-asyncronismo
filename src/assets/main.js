81de45822ce34ee76e01980a67bec2fd13db3551
germa099 <germainramirez11@gmail.com>
2023-03-24 10:05:06 -0600

    proyecto terminado y subido

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,7 +1,7 @@
-const content = null || document.getElementById("content");
+const content = null || document.querySelector("#content");
 
 const API =
-  "https://youtube138.p.rapidapi.com/channel/videos/?id=UCLvKYB8zy1N485JYWRopirw&filter=videos_latest&hl=en&gl=US";
+  "https://youtube138.p.rapidapi.com/channel/videos/?id=UCtwslQPB_xJfaFuZv0OETNw&filter=videos_latest&hl=en&gl=US";
 
 const options = {
   method: "GET",
@@ -19,30 +19,30 @@ async function conseguirDatos(url) {
 
 (async () => {
   try {
-    const videos = await conseguirDatos(API);
-    let view = `
-    ${videos.contents
+    const datos = await conseguirDatos(API);
+    let view = `${datos.contents
       .map(
-        (video) => `
-<div class="group relative">
-            <div
-              class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none"
-            >
-              <img src="${video.contents.videos.thumbnails.url}" alt="" class="w-full" />
-            </div>
-            <div class="mt-4 flex justify-between">
-              <h3 class="text-sm text-gray-700">
-                <span aria-hidden="true" class="absolute inset-0"></span>
-                title
-              </h3>
-            </div>
-          </div>`
+        (i) => `         
+    <div class="group relative">
+      <div
+            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+            <a href="https://www.youtube.com/watch?v=${i.video.videoId}&ab_channel=KingsLeagueInfoJobs" target="_blank">
+            <img src="${i.video.thumbnails[3].url}" alt="${i.video.title}" class="w-full">
+            </a>
+          </div>
+          <div class="mt-4 flex justify-between">
+            <a href="https://www.youtube.com/watch?v=${i.video.videoId}&ab_channel=KingsLeagueInfoJobs" target="_blank"
+            <h3 class="text-sm text-gray-700">
+              <span aria-hidden="true" class="absolute inset-0"></span>
+              ${i.video.title}
+            </h3>
+            </a>
+      </div>
+    </div>`
       )
-      .slice(0, 4)
-      .join("")}
-          `;
+      .join("")}`;
     content.innerHTML = view;
-  } catch (err) {
-    console.log(err);
+  } catch (er) {
+    console.log(er);
   }
 })();
